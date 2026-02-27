6efc2cccd81d3b3999e1840bd880ef901cd86d16
merrygoround-of-life <merrygoroundof@naver.com>
2026-02-27 11:54:41 +0900

    Initialize Kotlin Spring Boot project

---
+++ src/test/kotlin/com/plantrue/example/fuelradar/FuelRadarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.plantrue.example.fuelradar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FuelRadarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
